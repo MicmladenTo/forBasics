23ba57f3e0082a5371ecaa7463706039b9f4417e
Mladen <42699553+MicmladenTo@users.noreply.github.com>
2021-02-10 19:06:30 +0100

    Add files via upload

---
+++ Zadatak1.java
@@ -0,0 +1,18 @@
+package forBasics;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for (i = 1; i <= 20; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Kraj programa.");
+	}
+
+}
+//Napisati program koji stampa sve brojeve
+//od 1 do 20 pomocu for petlje
---
+++ Zadatak2.java
@@ -0,0 +1,17 @@
+package forBasics;
+
+public class Zadatak2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 20; i >= 1; i-=3) {
+			System.out.println("Program:" + i);
+		}
+		System.out.println("Kraj programa.");
+	}
+
+}
+//Napisati program koji stampa svaki treci broj
+//od 20 do 1 pomocu for petlje
